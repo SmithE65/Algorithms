a28204122721879855083346483c777905e5d590
Eric Smith <ydrive65@yahoo.com>
2022-03-11 18:16:37 -0500

    leetcode 1-3

---
+++ Alg.Net.Tests/LeetCodeTests.cs
@@ -0,0 +1,178 @@
+﻿using Xunit;
+
+namespace Alg.Net.Tests;
+
+public class LeetCodeTests
+{
+
+    #region TwoSort
+
+    [Fact]
+    public void TwoSortEx1()
+    {
+        // Arrange
+        var nums = new int[] { 2, 7, 11, 15 };
+        var target = 9;
+
+        // Act
+        var result = LeetCode.TwoSum(nums, target);
+
+        // Assert
+        Assert.Equal(2, result.Length);
+        Assert.Equal(target, nums[result[0]] + nums[result[1]]);
+    }
+
+    [Fact]
+    public void TwoSortEx2()
+    {
+        // Arrange
+        var nums = new int[] { 3, 2, 4 };
+        var target = 6;
+
+        // Act
+        var result = LeetCode.TwoSum(nums, target);
+
+        // Assert
+        Assert.Equal(2, result.Length);
+        Assert.Equal(target, nums[result[0]] + nums[result[1]]);
+    }
+
+    [Fact]
+    public void TwoSortEx3()
+    {
+        // Arrange
+        var nums = new int[] { 3, 3 };
+        var target = 6;
+
+        // Act
+        var result = LeetCode.TwoSum(nums, target);
+
+        // Assert
+        Assert.Equal(2, result.Length);
+        Assert.Equal(target, nums[result[0]] + nums[result[1]]);
+    }
+
+    #endregion
+
+    #region AddTwoNumbers
+
+    [Fact]
+    public void AddTwoNumbersEx1()
+    {
+        // Arrange
+        var l1 = CreateFromArray(new int[] { 2, 4, 3 });
+        var l2 = CreateFromArray(new int[] { 5, 6, 4 });
+
+        // Act
+        var result = LeetCode.AddTwoNumbers(l1, l2);
+
+        // Assert
+        var expected = CreateFromArray(new int[] { 7, 0, 8 });
+        while (expected != null)
+        {
+            Assert.Equal(expected.val, result.val);
+            expected = expected.next;
+            result = result.next;
+        }
+    }
+
+    [Fact]
+    public void AddTwoNumbersEx2()
+    {
+        // Arrange
+        var l1 = CreateFromArray(new int[] { 0 });
+        var l2 = CreateFromArray(new int[] { 0 });
+
+        // Act
+        var result = LeetCode.AddTwoNumbers(l1, l2);
+
+        // Assert
+        var expected = CreateFromArray(new int[] { 0 });
+        while (expected != null)
+        {
+            Assert.Equal(expected.val, result.val);
+            expected = expected.next;
+            result = result.next;
+        }
+    }
+
+    [Fact]
+    public void AddTwoNumbersEx3()
+    {
+        // Arrange
+        var l1 = CreateFromArray(new int[] { 9, 9, 9, 9, 9, 9, 9 });
+        var l2 = CreateFromArray(new int[] { 9, 9, 9, 9 });
+
+        // Act
+        var result = LeetCode.AddTwoNumbers(l1, l2);
+
+        // Assert
+        var expected = CreateFromArray(new int[] { 8, 9, 9, 9, 0, 0, 0, 1 });
+        while (expected != null)
+        {
+            Assert.Equal(expected.val, result.val);
+            expected = expected.next;
+            result = result.next;
+        }
+    }
+
+    private static ListNode CreateFromArray(int[] a)
+    {
+        var root = new ListNode { val = a[0] };
+        var next = root;
+
+        for (int i = 1; i < a.Length; i++)
+        {
+            next.next = new ListNode { val = a[i] };
+            next = next.next;
+        }
+
+        return root;
+    }
+
+    #endregion
+
+    #region LengthOfLongestSubstring
+
+    [Fact]
+    public void LengthOfLongestSubstringEx1()
+    {
+        // Arrange
+        var s = "abcabcbb";
+
+        // Act
+        var result = LeetCode.LengthOfLongestSubstring(s);
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void LengthOfLongestSubstringEx2()
+    {
+        // Arrange
+        var s = "bbbbb";
+
+        // Act
+        var result = LeetCode.LengthOfLongestSubstring(s);
+
+        // Assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void LengthOfLongestSubstringEx3()
+    {
+        // Arrange
+        var s = "pwwkew";
+
+        // Act
+        var result = LeetCode.LengthOfLongestSubstring(s);
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+
+    #endregion
+
+}
---
+++ Alg.Net/LeetCode.cs
@@ -0,0 +1,113 @@
+﻿namespace Alg.Net;
+
+public static class LeetCode
+{
+    /// <summary>
+    /// 1. Two Sum
+    /// Given an array of integers nums and an integer target,
+    /// return indices of the two numbers such that they add up to target.
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        // Brute force
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        throw new ArgumentException("target cannot be reached with provided nums");
+    }
+
+    /// <summary>
+    /// You are given two non-empty linked lists representing two non-negative integers.
+    /// The digits are stored in reverse order, and each of their nodes contains a single digit.
+    /// Add the two numbers and return the sum as a linked list.
+    /// </summary>
+    /// <param name="l1"></param>
+    /// <param name="l2"></param>
+    /// <returns></returns>
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        return ToListNode(ToInt(l1) + ToInt(l2));
+
+        static int ToInt(ListNode listNode)
+        {
+            int i = 1;
+            int result = 0;
+
+            while (listNode != null)
+            {
+                result += listNode.val * i;
+                i *= 10;
+                listNode = listNode.next;
+            }
+
+            return result;
+        }
+
+        static ListNode ToListNode(int i)
+        {
+            var array = i.ToString().Select(x => int.Parse(x.ToString())).Reverse().ToArray();
+
+            var root = new ListNode() { val = array[0] };
+            var next = root;
+
+            for (int j = 1; j < array.Length; j++)
+            {
+                next.next = new ListNode { val = array[j] };
+                next = next.next;
+            }
+
+            return root;
+        }
+    }
+
+    /// <summary>
+    /// Given a string s, find the length of the longest substring without repeating characters.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length < 2)
+        {
+            return s.Length;
+        }
+
+        int i;
+        var h = new HashSet<char>();
+
+        for (i = 0; i < s.Length; i++)
+        {
+            if (h.Contains(s[i]))
+            {
+                break;
+            }
+
+            h.Add(s[i]);
+        }
+
+        var j = LengthOfLongestSubstring(s.Substring(1));
+        return i > j ? i : j;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
